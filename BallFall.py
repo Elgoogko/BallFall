48434e281e85a6d11b5f44047d8e9143121d3dbe
Elgoogko <boicosb@gmail.com>
2025-05-31 14:51:04 +0200

    update

--- BallFall.py
+++ BallFall.py
@@ -1,79 +1,59 @@
 import pygame
 from pygame.locals import *
 import time
-import math
+import numpy as np
 
 # Game display properties
 DISPLAY = pygame.display.set_mode((500, 800))
+DISPLAY.fill(pygame.Color(0,0,0))
 
 # Game engine properties
 FPS = pygame.time.Clock()
 FPS.tick(60)
+TOTAL_FRAMES = 60 * 100
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, initVelocity : list[float], *groups):
+        super().__init__(*groups)
+        self.position = DISPLAY.get_rect().center
+        self.velocity = initVelocity
+        self.radius = [25, 20]
+        pygame.draw.circle(DISPLAY, (255,255,255), self.position, self.radius[0])
+        pygame.draw.circle(DISPLAY, (255,0,0), self.position, self.radius[1])
+
+    def update(self, *args, **kwargs):
+        self.velocity[1]+=0.3
+        self.position = np.add(self.position, self.velocity)
+
+        pygame.draw.circle(DISPLAY, (255,255,255), self.position, self.radius[0])
+        pygame.draw.circle(DISPLAY, (255,0,0), self.position, self.radius[1])
+
+class Halo(pygame.sprite.Sprite):
+    def __init__(self, radius : int, speed : int, *groups):
+        super().__init__(*groups)
+        self.position = [DISPLAY.get_rect().center[0],DISPLAY.get_rect().center[1]] 
+        self.radius = radius
+        self.speed = speed
+        
+        pygame.draw.circle(DISPLAY, (255,255,255), self.position, self.radius, 3, False, False, False, True)
+
+    def update(self, *args, **kwargs):
+        pygame.draw.circle(DISPLAY, (255,255,255), self.position, self.radius, 3, False)
+
+ball = Ball([-0.3,0])
+halo = Halo(250, 10)
 
 # Lauch pygame app
 pygame.init()
 
-class Ball(pygame.sprite.Sprite):
-    def __init__(self, *groups):
-        super().__init__(*groups)
-        self.radius = 25
-        self.image = pygame.Surface((self.radius*2, self.radius*2))
-        pygame.draw.circle(self.image, (255,255,255), (self.radius, self.radius), self.radius)
-        pygame.draw.circle(self.image, (255,0,0), (self.radius, self.radius), self.radius-5)
-        self.rect = self.image.get_rect()
-        self.velocity = 1
-        surface_rect = DISPLAY.get_rect()
-        self.rect.center = surface_rect.center
-        DISPLAY.blit(self.image, self.rect)
-
-
-    def update(self, *args, **kwargs):
-         self.rect.y += self.velocity
-         if self.rect.bottom > DISPLAY.get_height():
-             self.rect.bottom = DISPLAY.get_height()
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
-
-class halo(pygame.sprite.Sprite):
-    def __init__(self, radius, speed, *groups):
-        super().__init__(*groups)
-        self.radius = radius
-        self.speed = speed
-        self.image = pygame.Surface((self.radius*2, self.radius*2), pygame.SRCALPHA)
-        rect = pygame.Rect(0, 0, self.radius*2, self.radius*2)
-        pygame.draw.arc(self.image, (255,255,255), rect, 0, math.radians(180), 5)
-        self.rect = self.image.get_rect()
-        surface_rect = DISPLAY.get_rect()
-        self.rect.center = surface_rect.center
-        DISPLAY.blit(self.image, self.rect)
-
-    def update(self, *args, **kwargs):
-        self.image = pygame.transform.rotate(self.image, 1)
-        self.rect = self.image.get_rect(center=self.rect.center)
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
-
-ball = Ball()
-halo1 = halo(100, 0)
-
-while True:
-    pressed_keys = pygame.key.get_pressed()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            break
-        if(pressed_keys[K_KP_ENTER]):
-            ball = Ball()
-            print("ball Spawn")
-
+while pygame.time.get_ticks() < TOTAL_FRAMES:
     DISPLAY.fill(pygame.Color(0,0,0))
-
+    dist = np.sqrt((ball.position[0] - DISPLAY.get_rect().center[0])**2 + (ball.position[1] - DISPLAY.get_rect().center[1])**2)+ball.radius[0]
+    if(dist > halo.radius):
+        ball.velocity[1] = -ball.velocity[1]
     ball.update()
-    #halo1.update()
-
-    ball.draw(DISPLAY)
-    halo1.draw(DISPLAY)
-    
+    halo.update()
+    pygame.time.delay(10)
     pygame.display.update()
+
+pygame.quit()
